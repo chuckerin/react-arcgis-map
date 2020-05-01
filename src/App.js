a3c435881ea9f500371bfc4d944cfdb143afe70c
Chuck Black <CBlack@ADEV.LOCAL>
2020-05-01 14:37:12 -0500

    freking the map component won't recognize the state change

--- src/App.js
+++ src/App.js
@@ -3,27 +3,11 @@ import ReactMap from './components/react-map/react-map';
 import './App.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {baseMap: 'streets-night-vector'};
-    //streets-night-vector  hybrid
-  }
-
   render() {
-    console.log('App.render => ' + this.state.baseMap);
     return (
-      <div className='full-size'>
-        {/* <input className='mapType' type='text' placeholder='streets' 
-          onChange={e => this.setState({ baseMap: e.target.value })} /> */}
-        <ReactMap baseMap={this.state.baseMap} />
-      </div>
+        <ReactMap />
     );
   }
-}
-
-const handleChange = (event) => {
-  console.log(event.target.value);
-  this.state({baseMap: event.target.value});
 }
 
 export default App;
--- src/components/react-map/react-map.jsx
+++ src/components/react-map/react-map.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Map } from '@esri/react-arcgis';
 
 import './react-map.css';
 
-const ReactMap = (baseMap) => {
-    console.log('react-map => ' + baseMap.baseMap);
-    return <Map
-        mapProperties={{ basemap: baseMap.baseMap }}
-        viewProperties={{
-            center: [-93, 39],
-            zoom: 5
-        }}/>
+const ReactMap = () => {
+    const [baseMap, setBaseMap] = useState('streets-navigation-vector');
+    const handleChange = (e) => {
+        console.log(e.target.value);
+        setBaseMap(e.target.value);
+    };
+
+    return (
+        <div style={{height: '600px', width: '600px'}} >
+            <select 
+                style={{position: 'absolute', left: '500px', top: '10px', zIndex: '999'}}
+                value={baseMap}
+                onChange={handleChange}
+            >
+                <option value='streets-night-vector'>nightime...</option>
+                <option value='streets-navigation-vector'>DAYTIME!!!</option>
+                <option value='hybrid'>Hybrid</option>
+                <option value='streets'>Streets</option>
+                <option value='topo'>Topography</option>
+                <option value='satellite'>Satellite</option>
+            </select>
+            <Map 
+                mapProperties={{ basemap: baseMap }}
+                viewProperties={{
+                    center: [-93, 39],
+                    zoom: 5
+                }}
+            />
+        </div>
+    );
 };
 
 export default ReactMap;
